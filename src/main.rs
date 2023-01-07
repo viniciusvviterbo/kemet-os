a7312af966d687020ae0311ee3ecb29a6d129992
Vinícius Viterbo <v.viterbo@vp6.com.br>
2023-01-07 20:56:34 -0300

    3: VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,21 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
+    println!("Hello World{}", "!!!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // panic!("Some message info");
 
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
